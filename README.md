191d69cd13601cdf062c1d2b643c7de7d6622bd1
Scott Newman <snewman18@gmail.com>
2019-06-13 09:44:28 -0600

    Make the file name optional if you only want console logging

--- README.md
+++ README.md
@@ -13,13 +13,18 @@ Setting up EZLog is very simple:
 ```
 import ezlog
 
-logger = ezlog.Logger('<filename>', console_level='debug|info|warn|error', file_level='debug|info|warn|error')
+logger = ezlog.Logger(file_name='<filename>', console_level='debug|info|warn|error', file_level='debug|info|warn|error')
 
 logger.info('This is an informational message')
 logger.debug('This is a debugging message')
 ```
 
 _Note: The console level defaults to `debug`, the file level defaults to `info`._
+
+If you don't want file-level logging, simply pass `None` to the constructor and you will only get console-level logging:
+```
+logger = ezlog.Logger(file_name=None, console_level='debug|info|warn|error', file_level='debug|info|warn|error')
+```
 
 ## Contributing
 
--- ezlog/ezlog.py
+++ ezlog/ezlog.py
@@ -11,9 +11,10 @@ class Logger(object):
 
     def __init__(self, file_name=None, file_level='info', console_level='debug'):
 
-        assert file_name is not None, 'Log file name cannot be blank'
-        assert file_level in LOG_LEVELS.keys(), 'Invalid file logging level'
         assert console_level in LOG_LEVELS.keys(), 'Invalid console logging level'
+
+        if file_name is not None:
+            assert file_level in LOG_LEVELS.keys(), 'Invalid file logging level'
 
         # Store our log level so we can look it up later
         self.file_log_level = LOG_LEVELS[file_level]
@@ -27,17 +28,18 @@ class Logger(object):
         date_format = '%Y-%m-%dT%H:%M:%S'
         fmt = logging.Formatter(log_format, datefmt=date_format)
 
-        # Set up file logging
-        fh = logging.FileHandler(file_name)
-        fh.setLevel(self.file_log_level)
-        fh.setFormatter(fmt)
-        self.logger.addHandler(fh)
-
         # Set up console logging
         ch = logging.StreamHandler()
         ch.setLevel(self.console_log_level)
         ch.setFormatter(fmt)
         self.logger.addHandler(ch)
+
+        # Set up file logging (if a file name was specified)
+        if file_name:
+            fh = logging.FileHandler(file_name)
+            fh.setLevel(self.file_log_level)
+            fh.setFormatter(fmt)
+            self.logger.addHandler(fh)
 
 
     def info(self, msg):
