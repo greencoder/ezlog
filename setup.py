f74a264cfa3604705298d13d5f6e702fa03e4c08
Scott Newman <snewman18@gmail.com>
2019-10-12 09:48:40 -0600

    Bump version to 1.02 for PyPi

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='EZLogs',
-    version='1.0',
+    version='1.02',
     packages=['ezlogs',],
     license='MIT',
     description='A simple wrapper around the Python logging module',
     url='https://github.com/greencoder/ezlogs',
-    download_url='https://github.com/greencoder/ezlogs/archive/1.0.tar.gz',
+    download_url='https://github.com/greencoder/ezlogs/archive/1.02.tar.gz',
     author='Scott Newman',
     author_email='snewman18@gmail.com',
 )
